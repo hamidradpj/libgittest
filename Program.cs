ad523212899ef335a7e28c2e6e9150b8774200bf
hamidradpj <hamidradpj@gmail.com>
2021-09-01 16:23:35 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using LibGit2Sharp;
+
+namespace libgittest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string gitPath = Repository.Init("./");
+            using (var repo = new Repository(gitPath))
+            {
+                // Add Master Branch
+                Identity identity = new Identity("Hamid", "h.irannejad@parsjahd.com");
+
+                repo.CreateBranch("Hamid",repo.Commit("Master branch created",new Signature(identity,DateTimeOffset.Now), new Signature(identity, DateTimeOffset.Now)));
+                foreach (var b in repo.Branches)
+                {
+                    Console.WriteLine($"{b.FriendlyName}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
